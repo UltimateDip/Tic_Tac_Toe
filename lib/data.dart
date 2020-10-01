897ebb7e467a2e24c98399458ae34594e78c9ae4
UltimateDip <68418035+UltimateDip@users.noreply.github.com>
2020-10-01 14:22:47 +0530

    fixed the after winscreen issue

--- lib/data.dart
+++ lib/data.dart
@@ -16,26 +16,4 @@ var player = playerCopy,
     image = imageCopy,
     tileDetails = List.from(tileDetailsCopy);
 
-// tileDetailsCopy = [
-// [0, 11, true],
-// [1, 12, true],
-// [2, 13, true],
-// [3, 14, true],
-// [4, 15, true],
-// [5, 16, true],
-// [6, 17, true],
-// [7, 18, true],
-// [8, 19, true]
-// ];
 
-// tileDetailsCopy = [
-// [0, 11],
-// [1, 12],
-// [2, 13],
-// [3, 14],
-// [4, 15],
-// [5, 16],
-// [6, 17],
-// [7, 18],
-// [8, 19]
-// ];
